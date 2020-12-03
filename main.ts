81bf6d12af2c560d7a7b531f68205f9a17f3c7b3
code-ninja-account-GT <75400071+code-ninja-account-GT@users.noreply.github.com>
2020-12-02 19:33:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let enemy: Sprite = null
+let player = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(player)
+info.setLife(3)
+tiles.setTilemap(tilemap`level`)
+scene.cameraFollowSprite(player)
+game.onUpdateInterval(500, function () {
+    enemy = sprites.create(img`
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c c . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f c 3 b c 3 b c f b b c c c . 
+        f c b b b b b b c f b c b c c . 
+        c c 1 b b b 1 b c b b c b b c . 
+        c b b b b b b b b b c c c b c . 
+        c b 1 f f 1 c b b c c c c c . . 
+        c f 1 f f 1 f b b b b f c . . . 
+        f f f f f f f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 2 b b b c f . . . . 
+        . . f 2 2 2 b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    enemy.setFlag(SpriteFlag.DestroyOnWall, true)
+    enemy.setVelocity(-50, 0)
+    tiles.placeOnRandomTile(enemy, sprites.dungeon.greenOuterEast0)
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,47 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`10002000090a0a0a0a0a0a0a0a0a0a0a0a0a0a07050101010b0b0b0b0b0101010101010405010101010b0b0b0b0101010101010405010101010101010101010101010104050101010101010101010101010101040501010101010101010101010101010405020c0202020c02010101010101010405010101010101010101010101010104050101010101010101010101010101040501010101010101020c020202020c0405010101010101010101010101010104050101010101010101010101010101040501010101010101010101010101010405010101010101010101010101010104050101010101010101010101010101040501010101010101010101010101010405010101010101010101040101010104050101010101010101010401010101040502020c020202020c020301010101040501010101010101010101010101010405010101010101010101010101010104050101010101010101010101010101040501010101010501010101010101010405010101010105010101010101010104050101010101050101010101010101040501010101010602020c0202020c0204050101010101010101010d0d0e0d0d04050101010101010101010d0d0d0d0d04050101010101010101010d0d0d0d0d04050101010101010101010d0d0d0d0d04050101010101010101010d0d0d0d0d0406080808080808080808080808080808`, img`
+2222222222222222
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+22222222.......2
+2..............2
+2..............2
+2.......22222222
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2.........2....2
+2.........2....2
+22222222222....2
+2..............2
+2..............2
+2..............2
+2.....2........2
+2.....2........2
+2.....2........2
+2.....2222222222
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2222222222222222
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight4,sprites.dungeon.greenOuterNorth2,sprites.castle.tileGrass2,sprites.dungeon.chestClosed], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
